f4ea151ac4be92e361fa8ce6d38f3f4d4e46b163
osamaHamad0 <>
2021-05-06 16:38:09 +0000

    fix linter error

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 // this will run our find trapezoid area function
 document.getElementById('calculate-button').addEventListener('click', findTrapezoidArea)
 
-function findTrapezoidArea (){
+function findTrapezoidArea () {
   // we are assigning our variables with the user input
-  let a = document.getElementById('a').value;
-  let b = document.getElementById('b').value;
-  let h = document.getElementById('h').value;
+  let a = document.getElementById('a').value
+  let b = document.getElementById('b').value
+  let h = document.getElementById('h').value
 
   // trimming white spaces off of the variables
   // this will help with detecting the blank variable error
@@ -15,54 +15,54 @@ function findTrapezoidArea (){
   h = h.trim()
 
   // creating an array with our inputs to help parse through them
-  inputs = [a, b, h];
+  const inputs = [a, b, h]
 
   // parsing through our inputs to check for forbidden inputs
   // strings, null, numbers less than or equal to zero are forbidden
-  for (let i = 0; i < inputs.length; i++){
+  for (let i = 0; i < inputs.length; i++) {
     // this detects values that are Not a Number
-    if (isNaN(inputs[i])){
-      document.getElementById('answer').innerText = 'Value Error';
+    if (isNaN(inputs[i])) {
+      document.getElementById('answer').innerText = 'Value Error'
 
       // setting a time out so that alert comes after html change
       setTimeout(function() {
-      alert(`Value Error: \'${inputs[i]}\' is not a number`);
+        alert(`Value Error: '${inputs[i]}' is not a number`)
       }, 10)
-      return 0;
+      return 0
       } 
 
-      // this detects blank variables
-      else if (inputs[i] === ''){
-      document.getElementById('answer').innerText = 'Value Error';
+        // this detects blank variables
+        else if (inputs[i] === '') {
+      document.getElementById('answer').innerText = 'Value Error'
 
       // setting a time out so that alert comes after html change
       setTimeout(function() {
-      alert(`Value Error: all variables must be filled`);
+        alert(`Value Error: all variables must be filled`)
       }, 10)
-      return 0;
+      return 0
       } 
 
       // this detects values equal to zero or any negative number
-      else if (inputs[i] <= 0){
-      document.getElementById('answer').innerText = 'Math Error';
+      else if (inputs[i] <= 0) {
+      document.getElementById('answer').innerText = 'Math Error'
 
       // setting a time out so that alert comes after html change
       setTimeout(function() {
-      alert(`Math Error: cannot have value less than or equal to 0 (${inputs[i]})`);
+      alert(`Math Error: cannot have value less than or equal to 0 (${inputs[i]})`)
       }, 10)
-      return 0;
+      return 0
     }
   }
 
   // the input is a recieved as type str so we converted it to float
-  a = parseFloat(a);
-  b = parseFloat(b);
-  h = parseFloat(h);
+  a = parseFloat(a)
+  b = parseFloat(b)
+  h = parseFloat(h)
 
   // we are calculating the area using the trapezoid area formula
   // A = ((a + b) / 2) * h
-  let area = ((a + b) / 2) * h;
+  const area = ((a + b) / 2) * h
 
   // we are changing the content of the paragraph with id answer to the area we got
-  document.getElementById('answer').innerText = area;
+  document.getElementById('answer').innerText = area
 }
